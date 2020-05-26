cc8a9534cacc7d06ac28c9769ac6b7ca14a0d788
vogier <contact@vogier.fr>
2020-05-26 19:25:25 +0200

    Add build.sh script, and remove g++ flags requiring a recent version.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Build the c++ extension using CMake.
+# This script will not install the extension.
+# To install the extension (and build it if it not already) run
+#     python -m pip install .
+# in this directory.
+
+
+
+cd "$(dirname "$0")" &&
+cmake -S . -B "./build" -DCMAKE_BUILD_TYPE=${1:Release} &&
+cmake --build "./build"
+
